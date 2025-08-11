b67f88ffbb7f11722c8a3861cc639b05cfcee680
Lan-Anh-2910 <153609816+Lan-Anh-2910@users.noreply.github.com>
2025-08-11 14:52:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px  # <-- thêm dòng này
 
 # --- Load token ---
 mapbox_token = st.secrets["mapbox_token"]
